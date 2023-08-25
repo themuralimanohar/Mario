f0115e04588d68e7e815bffb903242188db4f186
Muralimanohar <murali@kissflow.com>
2023-08-25 15:56:15 +0530

    Mario changes

--- js/script.js
+++ js/script.js
@@ -1,263 +1,271 @@
-"use strict";
-class PRESTimeline {
-    constructor(target, color) {
-        // this.__process_stylesheet(document.styleSheets[0]);
-        this.base = target;
-        this.color = color;
-        // console.log(this.color)
-        this.periodContainer = $(this.base).find('.periods-container');
-        this.cardContainer = $(this.base).find('.cards-container');
-        this.timelineNodeContainer = $(this.base).find('.timeline-container .timeline');
-        // this.activePeriod = $(this.base).find('.periods-container section.active')
-        this._parseData();
-        this._initialColor();
-        this._generateTimeline();
-        this._setStateClasses();
-        this._assignBtn();
-        this._adjustPeriodContainer();
-        this._adjustCardContainer();
-        // console.log(this.cardData)
+const TWO_PI = Math.PI * 2;
+const HALF_PI = Math.PI * 0.5;
+
+// canvas settings
+var viewWidth = 512,
+    viewHeight = 350,
+    drawingCanvas = document.getElementById("drawing_canvas"),
+    ctx,
+    timeStep = (1/60);
+
+Point = function(x, y) {
+    this.x = x || 0;
+    this.y = y || 0;
+};
+
+Particle = function(p0, p1, p2, p3) {
+    this.p0 = p0;
+    this.p1 = p1;
+    this.p2 = p2;
+    this.p3 = p3;
+
+    this.time = 0;
+    this.duration = 1 + Math.random() * 2;
+    this.color =  '#' + Math.floor((Math.random() * 0xffffff)).toString(16);
+
+    this.w = 8;
+    this.h = 6;
+
+    this.complete = false;
+};
+
+Particle.prototype = {
+    update:function() {
+        this.time = Math.min(this.duration, this.time + timeStep);
+
+        var f = Ease.outCubic(this.time, 0, 1, this.duration);
+        var p = cubeBezier(this.p0, this.p1, this.p2, this.p3, f);
+
+        var dx = p.x - this.x;
+        var dy = p.y - this.y;
+
+        this.r =  Math.atan2(dy, dx) + HALF_PI;
+        this.sy = Math.sin(Math.PI * f * 10);
+        this.x = p.x;
+        this.y = p.y;
+
+        this.complete = this.time === this.duration;
+    },
+    draw:function() {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.r);
+        ctx.scale(1, this.sy);
+
+        ctx.fillStyle = this.color;
+        ctx.fillRect(-this.w * 0.5, -this.h * 0.5, this.w, this.h);
+
+        ctx.restore();
     }
-    
-    _parseData() {
-        let base = this.base;
-        let periods = $(base).find('.periods-container section');
-        for (let section of periods) {
-            section.period = $(section).attr('period');
-            section.index = $(section).index();
-        }
-        // console.log(periods)
-        this.periodData = periods;
-        let data = $(base).find('.cards-container section');
-        // console.log(data)
-        for (let section of data) {
-            section.period = $(section).attr('period');
-            section.index = $(section).index();
-        }
-        // console.log(data)
-        this.cardData = data;
-        // #assign initial entry point (active items)
-        this.activePeriod = this.periodData[0];
-        this.activePeriodIndex = 0;
-        this.activeCard = this.cardData[0];
-        this.activeCardIndex = 0;
+};
+
+Loader = function(x, y) {
+    this.x = x;
+    this.y = y;
+
+    this.r = 24;
+    this._progress = 0;
+
+    this.complete = false;
+};
+
+Loader.prototype = {
+    reset:function() {
+        this._progress = 0;
+        this.complete = false;
+    },
+    set progress(p) {
+        this._progress = p < 0 ? 0 : (p > 1 ? 1 : p);
+
+        this.complete = this._progress === 1;
+    },
+    get progress() {
+        return this._progress;
+    },
+    draw:function() {
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, -HALF_PI, TWO_PI * this._progress - HALF_PI);
+        ctx.lineTo(this.x, this.y);
+        ctx.closePath();
+        ctx.fill();
     }
-    _setStateClasses() {
-        // # periods
-        $(this.base).find('.periods-container section.active').removeClass('active');
-        $(this.base).find('.periods-container section.prev').removeClass('prev');
-        $(this.base).find('.periods-container section.next').removeClass('next');
-        // console.log("setclass: " + this.activePeriod.index)
-        $(this.activePeriod).addClass('active');
-        // console.log(this.activePeriod.index)
-        // this.activePeriodIndex = this.activePeriod.index
-        if ($(this.activePeriod).prev().length != 0) {
-            $(this.activePeriod).prev().addClass('prev');
-            $(this.base).find('.periods-container .btn-back').removeClass('hide');
-        }
-        else {
-            $(this.base).find('.periods-container .btn-back').addClass('hide');
-        }
-        if ($(this.activePeriod).next().length != 0) {
-            $(this.activePeriod).next().addClass('next');
-            $(this.base).find('.periods-container .btn-next').removeClass('hide');
-        }
-        else {
-            $(this.base).find('.periods-container .btn-next').addClass('hide');
-        }
-        // ## cards
-        $(this.base).find('.cards-container section.active').removeClass('active');
-        $(this.base).find('.cards-container section.prev').removeClass('prev');
-        $(this.base).find('.cards-container section.next').removeClass('next');
-        $(this.activeCard).addClass('active');
-        // this.activeCardIndex - this.activeCard.index
-        if ($(this.activeCard).prev().length != 0) {
-            $(this.activeCard).prev().addClass('prev');
-        }
-        if ($(this.activeCard).next().length != 0) {
-            $(this.activeCard).next().addClass('next');
-        }
-        // ## timeline 
-        $(this.base).find('.timeline li.active').removeClass('active');
-        // let findNode = $(this.base).find('.timeline ol li')[this.activeCard.index]
-        $(this.timelineData[this.activeCard.index]).addClass('active');
-        let timelineB = $(this.base).find('.timeline-container .btn-back');
-        let timelineN = $(this.base).find('.timeline-container .btn-next');
-        // console.log($(timelineN))
-        if (this.activeCardIndex === 0) {
-            timelineB.addClass('hide');
-        }
-        else {
-            timelineB.removeClass('hide');
-        }
-        if (this.activeCardIndex >= this.cardData.length - 1) {
-            timelineN.addClass('hide');
-        }
-        else {
-            timelineN.removeClass('hide');
-        }
+};
+
+// pun intended
+Exploader = function(x, y) {
+    this.x = x;
+    this.y = y;
+
+    this.startRadius = 24;
+
+    this.time = 0;
+    this.duration = 0.4;
+    this.progress = 0;
+
+    this.complete = false;
+};
+
+Exploader.prototype = {
+    reset:function() {
+        this.time = 0;
+        this.progress = 0;
+        this.complete = false;
+    },
+    update:function() {
+        this.time = Math.min(this.duration, this.time + timeStep);
+        this.progress = Ease.inBack(this.time, 0, 1, this.duration);
+
+        this.complete = this.time === this.duration;
+    },
+    draw:function() {
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.startRadius * (1 - this.progress), 0, TWO_PI);
+        ctx.fill();
     }
-    // ## timeline generater
-    _generateTimeline() {
-        // ## create node list
-        let htmlWrap = '<ol></ol>';
-        $(this.timelineNodeContainer).append(htmlWrap);
-        let wrap = $(this.timelineNodeContainer).find('ol');
-        let numNode = this.cardData.length;
-        for (let i = 0; i < numNode; i++) {
-            let c = this.cardData[i].color;
-            let el = wrap.append('<li class="' + this.cardData[i].period + '" style="border-color: ' + c + '"></li>');
-        }
-        // ## width of timeline
-        let nodeW = 200;
-        wrap.css('width', nodeW * numNode - 16);
-        let nodeList = $(this.base).find('.timeline ol li');
-        this.timelineData = nodeList;
-    }
-    // ## assign button actions
-    _assignBtn() {
-        let periodPrev = $(this.base).find('.periods-container .btn-back');
-        let periodNext = $(this.base).find('.periods-container .btn-next');
-        periodPrev.click(() => {
-            if (this.activePeriodIndex > 0) {
-                // console.log('prev')
-                this.activePeriodIndex -= 1;
-                this.activePeriod = this.periodData[this.activePeriodIndex];
-                this._chainActions('period');
-                this._setStateClasses();
-            }
-            this._adjustPeriodContainer();
-        });
-        periodNext.click(() => {
-            if (this.activePeriodIndex < this.periodData.length - 1) {
-                // console.log('next' + this.activePeriodIndex)
-                this.activePeriodIndex += 1;
-                this.activePeriod = this.periodData[this.activePeriodIndex];
-                this._chainActions('period');
-                this._setStateClasses();
-            }
-            this._adjustPeriodContainer();
-        });
-        let timelinePrev = $(this.base).find('.timeline-container .btn-back');
-        let timelineNext = $(this.base).find('.timeline-container .btn-next');
-        timelinePrev.click(() => {
-            if (this.activeCardIndex > 0) {
-                this.activeCardIndex -= 1;
-                this.activeCard = this.cardData[this.activeCardIndex];
-                this._chainActions('timeline');
-                this._setStateClasses();
-            }
-            this._adjustCardContainer();
-            this._adjustPeriodContainer();
-        });
-        timelineNext.click(() => {
-            if (this.activeCardIndex < this.cardData.length - 1) {
-                this.activeCardIndex += 1;
-                this.activeCard = this.cardData[this.activeCardIndex];
-                this._chainActions('timeline');
-                this._setStateClasses();
-            }
-            this._adjustCardContainer();
-            this._adjustPeriodContainer();
-        });
-        // ## assign each timeline li
-        for (let i = 0; i < this.timelineData.length; i++) {
-            $(this.timelineData[i]).click(() => {
-                this.activeCardIndex = this.cardData[i].index;
-                this.activeCard = this.cardData[this.activeCardIndex];
-                this._chainActions('timeline');
-                this._setStateClasses();
-                this._adjustCardContainer();
-                this._shiftTimeline();
-            });
-        }
-    }
-    // ## color ##
-    _initialColor() {
-        for (let i = 0; i < this.periodData.length; i++) {
-            let p = this.periodData[i].period;
-            this.periodData[i].color = this.color[p];
-            let temp = this.periodData[i];
-            $(temp).css('border-color', temp.color);
-            $(temp).find('.year').css('color', temp.color);
-            // ## color for timeline items, this part utilize the period name as class which will be add to the li later
-            // ### cross browser bug fix
-            let sbstyle = document.createElement("style");
-            document.head.appendChild(sbstyle);
-            // let sheet = document.styleSheets[0]
-            sbstyle.sheet.insertRule('li.' + p + '.active { background-color: ' + this.color[p] + ' !important } ', 0);
-            sbstyle.sheet.insertRule('li.' + p + '::before { background-color: ' + this.color[p] + ' } ', 0);
-            sbstyle.sheet.insertRule('li.' + p + '::after { background-color: ' + this.color[p] + ' } ', 0);
-        }
-        for (let i = 0; i < this.cardData.length; i++) {
-            let p = this.cardData[i].period;
-            this.cardData[i].color = this.color[p];
-            let temp = this.cardData[i];
-            $(temp).css('border-color', temp.color);
-            $(temp).find('.year').css('color', temp.color);
-        }
-    }
-    _adjustPeriodContainer() {
-        let activeH = $(this.activePeriod).outerHeight();
-        $(this.periodContainer).height(activeH);
-        console.log('top adjusted');
-    }
-    _adjustCardContainer() {
-        let activeH = $(this.activeCard).outerHeight() + 24;
-        $(this.cardContainer).height(activeH);
-        console.log('bot adjusted');
-    }
-    _shiftTimeline() {
-        // #### We need to fix this part if using this component in different sizes ####
-        let timelineW = $(this.base).find('.timeline-container').outerWidth();
-        let timelinePadding = 210;
-        let timelineCenter = 300;
-        let liWidth = 16;
-        let activeNodeX = $(this.timelineData[this.activeCardIndex]).position().left;
-        let finalPos = -activeNodeX + timelinePadding;
-        $(this.timelineNodeContainer).css('left', finalPos);
-        console.log(activeNodeX);
-    }
-    _chainActions(state) {
-        switch (state) {
-            case 'period':
-                console.log('period');
-                if (this.activePeriod.period != this.activeCard.period) {
-                    // ## find the closest li with the active period
-                    let ta = [];
-                    for (let i = 0; i < this.cardData.length; i++) {
-                        let temp = this.cardData[i];
-                        if (this.activePeriod.period === temp.period)
-                            ta.push(temp);
-                    }
-                    this.activeCard = ta[0];
-                    this.activeCardIndex = ta[0].index;
-                }
-                break;
-            case 'timeline':
-                console.log('timeline');
-                if (this.activeCard.period != this.activePeriod.period) {
-                    let ta;
-                    for (let i = 0; i < this.periodData.length; i++) {
-                        let temp = this.periodData[i];
-                        if (this.activeCard.period === temp.period)
-                            ta = temp;
-                    }
-                    this.activePeriod = ta;
-                    this.activePeriodIndex = ta.index;
-                }
-                break;
-        }
-        this._shiftTimeline();
-        this._adjustCardContainer();
+};
+
+var particles = [],
+    loader,
+    exploader,
+    phase = 0;
+
+function initDrawingCanvas() {
+    drawingCanvas.width = viewWidth;
+    drawingCanvas.height = viewHeight;
+    ctx = drawingCanvas.getContext('2d');
+
+    createLoader();
+    createExploader();
+    createParticles();
+}
+
+function createLoader() {
+    loader = new Loader(viewWidth * 0.5, viewHeight * 0.5);
+}
+
+function createExploader() {
+    exploader = new Exploader(viewWidth * 0.5, viewHeight * 0.5);
+}
+
+function createParticles() {
+    for (var i = 0; i < 128; i++) {
+        var p0 = new Point(viewWidth * 0.5, viewHeight * 0.5);
+        var p1 = new Point(Math.random() * viewWidth, Math.random() * viewHeight);
+        var p2 = new Point(Math.random() * viewWidth, Math.random() * viewHeight);
+        var p3 = new Point(Math.random() * viewWidth, viewHeight + 64);
+
+        particles.push(new Particle(p0, p1, p2, p3));
     }
 }
-// ## document load ##
-$(document).ready(function () {
-    let colorcode = {
-        'period1': '#fec541',
-        'period2': '#36d484',
-        'period3': '#32ccf4'
-    };
-    let timeline = new PRESTimeline($('#this-timeline'), colorcode);
-});
+
+function update() {
+
+    switch (phase) {
+        case 0:
+            loader.progress += (1/45);
+            break;
+        case 1:
+            exploader.update();
+            break;
+        case 2:
+            particles.forEach(function(p) {
+                p.update();
+            });
+            break;
+    }
+}
+
+function draw() {
+    ctx.clearRect(0, 0, viewWidth, viewHeight);
+
+    switch (phase) {
+        case 0:
+            loader.draw();
+            break;
+        case 1:
+            exploader.draw();
+            break;
+        case 2:
+            particles.forEach(function(p) {
+                p.draw();
+            });
+        break;
+    }
+}
+
+window.onload = function() {
+    initDrawingCanvas();
+    requestAnimationFrame(loop);
+};
+
+function loop() {
+    update();
+    draw();
+
+    if (phase === 0 && loader.complete) {
+        phase = 1;
+    }
+    else if (phase === 1 && exploader.complete) {
+        phase = 2;
+    }
+    else if (phase === 2 && checkParticlesComplete()) {
+        // reset
+        phase = 0;
+        loader.reset();
+        exploader.reset();
+        particles.length = 0;
+        createParticles();
+    }
+
+    requestAnimationFrame(loop);
+}
+
+function checkParticlesComplete() {
+    for (var i = 0; i < particles.length; i++) {
+        if (particles[i].complete === false) return false;
+    }
+    return true;
+}
+
+// math and stuff
+
+/**
+ * easing equations from http://gizma.com/easing/
+ * t = current time
+ * b = start value
+ * c = delta value
+ * d = duration
+ */
+var Ease = {
+    inCubic:function (t, b, c, d) {
+        t /= d;
+        return c*t*t*t + b;
+    },
+    outCubic:function(t, b, c, d) {
+        t /= d;
+        t--;
+        return c*(t*t*t + 1) + b;
+    },
+    inOutCubic:function(t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t*t + b;
+        t -= 2;
+        return c/2*(t*t*t + 2) + b;
+    },
+    inBack: function (t, b, c, d, s) {
+        s = s || 1.70158;
+        return c*(t/=d)*t*((s+1)*t - s) + b;
+    }
+};
+
+function cubeBezier(p0, c0, c1, p1, t) {
+    var p = new Point();
+    var nt = (1 - t);
+
+    p.x = nt * nt * nt * p0.x + 3 * nt * nt * t * c0.x + 3 * nt * t * t * c1.x + t * t * t * p1.x;
+    p.y = nt * nt * nt * p0.y + 3 * nt * nt * t * c0.y + 3 * nt * t * t * c1.y + t * t * t * p1.y;
+
+    return p;
+}
